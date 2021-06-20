6a1b347abb4988697a790860fbd81dd56a0c1050
Josias Junior <1001josias@gmail.com>
2021-06-20 14:19:23 -0300

    criação do componente Menu

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -3,13 +3,17 @@ import { Card } from '../../components/Card';
 import { Footer } from '../../components/Footer';
 import { Header } from '../../components/Header';
 import Input from '../../components/Input/input.js';
-
+import Menu from '../../components/Menu'
 import './style.scss';
 
 export const Search = () => {
     return (
         <>
-            <Header subtitle="Estes são os profissionais disponíveis."/>
+            <Header subtitle="Estes são os profissionais disponíveis.">
+            <Menu>
+                {[{item:'menu item ',url:'url test '},{item:'menu item ',url:'url test '},{item:'menu item ',url:'url test '},{item:'menu item ',url:'url test '}]}
+            </Menu>
+            </Header>
             <section className="search-content">
                 <section className="search-fields">
                     <Input inputStyle="input-small" pattern="text" field="area" subtitle="Qual profissional você precisa?"></Input>
