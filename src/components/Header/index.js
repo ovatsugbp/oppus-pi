ec8c3a67d9834e38d533b4c494c9d605c29866ea
Josias Junior <1001josias@gmail.com>
2021-06-22 23:19:42 -0300

    Adicionei o item Home ao Menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -10,6 +10,7 @@ export const Header = ({subtitle}) => {
         <header>
             <Menu>
                 {[
+                    {item:'Home',rout:'/'},
                     {item:'Meu perfil',rout:'/usuario'},
                 ]}
             </Menu>
