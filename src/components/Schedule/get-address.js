df1bde70db41cbdd4b4b59145171175b4901c11c
Josias Junior <1001josias@gmail.com>
2021-07-24 23:07:00 -0300

    Insere dados do endereço após preencher o cep

--- src/components/Schedule/get-address.js
+++ src/components/Schedule/get-address.js
@@ -1,11 +1,21 @@
 async function getAddress(cep){
-   try {
-      let res = await fetch(`https://brasilapi.com.br/api/cep/v2/${cep}`)
-      return await res.json()
-   } catch (error) {
-      console.log(error);
-       alert(error)
+   let response = await fetch(`https://brasilapi.com.br/api/cep/v2/${cep}`)
+   let data = await response.json()
+   console.log(data)
+   if(response.status === 200){
+      return {
+         status: response.status,
+         state: data.state,
+         city: data.city,
+         neighborhood: data.neighborhood
+      }
+   }else{
+      return {
+         status: response.status,
+         error: data.type
+      }
    }
+
 }
 
 export default getAddress 
