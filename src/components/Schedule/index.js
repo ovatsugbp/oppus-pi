6e17440d2a141c1149ae47ed50ab03428a335482
Geovana Espanhol <geovana.bespanhol@gmail.com>
2021-08-20 19:38:19 -0300

    Validação do componente schedule

--- src/components/Schedule/index.js
+++ src/components/Schedule/index.js
@@ -11,20 +11,78 @@ const Schedule = ({ id, handleClick }) => {
     const [state, setState] = useState()
     const [city, setCity] = useState()
     const [neighborhood, setNeighborhood] = useState()
+    const [schedule, setSchedule] = useState({
+        startTime: '',
+        finishTime: '',
+        locationCep: '',
+        locationUF: state,
+        locationDistrict: city,
+        locationCity: neighborhood
+    })
+
+    const [errors, setErrors] = useState({})
+    let isValid;
+
+    function validateSchedule() {
+        errors = {};
+
+        if(!values.startTime) {
+            errors.startTime = "Campo obrigatório";
+            isValid = false;
+        }
+        if(!values.finishTime) {
+            errors.finishTime = "Campo obrigatório";
+            isValid = false;
+        }
+        if(!values.locationCep) {
+            errors.locationCep = "Campo obrigatório";
+            isValid = false;
+        } else if(!/\d{8}/.test(locationCep)){
+            errors.locationCep = "Formato de CEP inválido (digite somente números)";
+            isValid = false;
+        }
+        if(!values.locationUF) {
+            errors.locationUF = "Campo obrigatório";
+            isValid = false;
+        }
+        if(!values.locationCity) {
+            errors.locationCity = "Campo obrigatório";
+            isValid = false;
+        }
+        if(!values.locationDistrict) {
+            errors.locationDistrict = "Campo obrigatório";
+            isValid = false;
+        }
+        else {
+            isValid = true;
+        }
+        setErrors({... errors});
+    }
+
+    const handleChange = e => {
+        setSchedule(
+            {...schedule, [e.target.name]: e.target.value}
+    );
+        console.log(schedule);
+        validateSchedule();
+    }
+
 
     return (
         <section key={id} className="form-content schedule">
             <div className="period">
                 <SelectInput subtitle="Dia da semana" field="week-day" prompt="Selecione" data={daysOfTheWeek}  id="id" label="label" value={day} onChange={(val) => setDay(val)}></SelectInput>
-                <Input field="start-hour" pattern="time" subtitle="Das" inputStyle="input-medium" />
-                <Input field="finish-hour" pattern="time" subtitle="Até" inputStyle="input-medium" />
+                <Input field="start-hour" pattern="time" inputValue={schedule.startTime} subtitle="Das" inputStyle="input-medium" onChange={handleChange} />
+                 <p className="error-message">{errors.startTime}</p>
+                <Input field="finish-hour" pattern="time" inputValue={schedule.finishTime} subtitle="Até" inputStyle="input-medium" onChange={handleChange} />
+                 <p className="error-message">{errors.finishTime}</p>
             </div>
             <div className="location-row1">
                 <Input field="location-cep" 
                 pattern="text" 
                 subtitle="CEP" 
                 inputStyle="input-medium" 
-                onInput={ 
+                onChange={ 
                     async(e)=> {
                         let cep = e.target.value
                         if(cep.length === 8){
@@ -38,15 +96,19 @@ const Schedule = ({ id, handleClick }) => {
                         }
                     }
             }/>
-                <Input field="location-UF" pattern="text" subtitle="UF" inputStyle="input-medium" inputValue={state}/>
+                <Input field="locationUF" pattern="text" subtitle="UF" inputStyle="input-medium" inputValue={state} onChange={handleChange} />
                 <button className="trash-bin-icon">
                     <DeleteOutlineOutlinedIcon onClick={handleClick} />
                 </button>
             </div>
+            <p className="error-message">{errors.locationCep}</p>
+            <p className="error-message">{errors.locationUF}</p>
             <div className="location-row2">
-                <Input field="location-district" pattern="text" subtitle="Bairro" inputStyle="input-medium" inputValue={neighborhood}/>
-                <Input field="location-city" pattern="text" subtitle="Cidade" inputStyle="input-medium" inputValue={city}/>
-                </div>
+                <Input field="location-district" pattern="text" subtitle="Bairro" inputStyle="input-medium" inputValue={neighborhood} onChange={handleChange} />
+                <Input field="location-city" pattern="text" subtitle="Cidade" inputStyle="input-medium" inputValue={city} onChange={handleChange} />
+            </div>
+            <p className="error-message">{errors.locationDistrict}</p>
+            <p className="error-message">{errors.locationcity}</p>
         </section>
     );
 }
